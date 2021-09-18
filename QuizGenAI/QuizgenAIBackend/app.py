9746ee5ba0fd805c4cf233d4609123a609cf7a35
Pranav Karmalkar <pranavkarmalkar@gmail.com>
2021-09-18 03:27:06 -0700

    Updated repo 2

---
+++ QuizGenAI/QuizgenAIBackend/app.py
@@ -0,0 +1,59 @@
+from flask import Flask, request, jsonify
+from tfgenerator import TrueFalsePreprocessing
+from models import Models
+from login_register import Authentication
+from flask_pymongo import PyMongo
+from connect import URI
+
+
+app = Flask(__name__)
+
+
+def connectdb(app):
+    mongodb_client = PyMongo(app, uri=URI)
+    db = mongodb_client.db
+    return db
+
+
+db = connectdb(app)
+
+models = Models()
+AllenNLPpredictor, GPT2tokenizer, GPT2model, BERT_model_tfquestions = models.generate_all_models()
+print("Hi", GPT2tokenizer)
+
+
+@app.route('/')
+def hello_world():
+    return 'This is my first API call!'
+
+
+@app.route("/registerUser", methods=['POST'])
+def register_user():
+    if request.method == 'POST':
+        user_details = request.get_json()
+        auth = Authentication()
+        code, json_message = auth.user_registration(db, user_details)
+        return json_message, code
+
+
+@app.route("/login", methods=['POST'])
+def login():
+    if request.method == 'POST':
+        user_details = request.get_json()
+        auth = Authentication()
+        code, json_message = auth.user_login(db, user_details)
+        return json_message, code
+
+
+@app.route("/true_false_questions", methods=["GET"])
+def tfgen():
+    if request.method == 'GET':
+        sentence_json = request.get_json()
+        sentence = sentence_json['sentence']
+        tfpre = TrueFalsePreprocessing(AllenNLPpredictor)
+        false_question = tfpre.tfdriver(sentence, GPT2tokenizer, GPT2model, BERT_model_tfquestions)
+        return jsonify(message=false_question)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
---
+++ QuizGenAI/QuizgenAIBackend/models.py
@@ -0,0 +1,36 @@
+from allennlp.predictors.predictor import Predictor
+from transformers import TFGPT2LMHeadModel, GPT2Tokenizer
+from sentence_transformers import SentenceTransformer
+from constants import LOGGER_FORMAT, ALLEN_NLP_MODEL, GPT2, BERT_FOR_TF_QUESTIONS
+import logging
+import inspect
+
+
+class Models:
+
+    def __init__(self):
+        self.log = logging.getLogger(self.__class__.__name__)
+        file_handler = logging.FileHandler(f'logs/{self.__class__.__name__}.log')
+        formatter = logging.Formatter(LOGGER_FORMAT)
+        file_handler.setFormatter(formatter)
+        self.log.addHandler(file_handler)
+
+    def true_false_questions(self):
+        try:
+            predictor = Predictor.from_path(ALLEN_NLP_MODEL)
+            GPT2tokenizer = GPT2Tokenizer.from_pretrained(GPT2)
+            GPT2model = TFGPT2LMHeadModel.from_pretrained(GPT2, pad_token_id=GPT2tokenizer.eos_token_id)
+            BERT_model = SentenceTransformer(BERT_FOR_TF_QUESTIONS)
+            # print(type(predictor), type(GPT2tokenizer), type(GPT2model), type(BERT_model))
+            return predictor, GPT2tokenizer, GPT2model, BERT_model
+        except Exception as e:
+            print("Hello")
+            self.log.debug(f"{inspect.currentframe().f_code.co_name} . Error: {e}")
+            return None, None, None, None
+
+    def generate_all_models(self):
+        AllenNLPpredictor, GPT2tokenizer, GPT2model, BERT_model_tfquestions = self.true_false_questions()
+        return AllenNLPpredictor, GPT2tokenizer, GPT2model, BERT_model_tfquestions
+
+models = Models()
+AllenNLPpredictor, GPT2tokenizer, GPT2model, BERT_model_tfquestions = models.generate_all_models()
